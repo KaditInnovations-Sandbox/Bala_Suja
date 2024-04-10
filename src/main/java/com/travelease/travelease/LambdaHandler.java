3e108439c129165ca1024124b15475e00ff2229b
Samynathan-S <samynathan.s@kaditinnovations.com>
2024-04-10 14:59:20 +0530

    lambda handler update

--- src/main/java/com/travelease/travelease/LambdaHandler.java
+++ src/main/java/com/travelease/travelease/LambdaHandler.java
@@ -2,17 +2,23 @@ package com.travelease.travelease;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.amazonaws.serverless.proxy.internal.model.AwsProxyRequest;
+import com.amazonaws.serverless.proxy.internal.model.AwsProxyResponse;
+import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class LambdaHandler implements RequestHandler<AwsProxyRequest, AwsProxyResponse> {
     private static SpringBootLambdaContainerHandler<AwsProxyRequest, AwsProxyResponse> handler;
+    private static AnnotationConfigApplicationContext appContext;
 
     static {
         try {
-            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(Application.class); }
-        catch (ContainerInitializationException ex){
-            throw new RuntimeException("Unable to load spring boot application",ex); }
+            appContext = new AnnotationConfigApplicationContext(Application.class);
+            handler = SpringBootLambdaContainerHandler.getAwsProxyHandler(appContext);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException("Unable to initialize Spring Boot application context", ex);
+        }
     }
 
     @Override
